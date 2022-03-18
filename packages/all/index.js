732f7a4823f0a40946e817c564fafc4a4bf4b2ed
k <40721155+linkdevk@users.noreply.github.com>
2022-03-18 00:59:04 -0700

    chore: cleanup

--- packages/all/index.js
+++ packages/all/index.js
@@ -2,19 +2,20 @@ module.exports = {
   extends: [
     '@kylegl/eslint-config-react',
     '@kylegl/eslint-config-vue',
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:testing-library/vue"
   ],
-  plugins: ['testing-library'],
-  rules: {
-    "testing-library/await-async-query": "error",
-    "testing-library/no-await-sync-query": "error",
-    "testing-library/no-debugging-utils": "warn",
-    "testing-library/no-dom-import": "off"
+  plugins: ["jest"],
+  env: {
+    "jest/globals": true
   },
   overrides: [
     {
       // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching files!
-      "files": ["**/tests/**/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      "files": ["**/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
       "extends": ["plugin:testing-library/vue"]
     },
   ],
 }
+
